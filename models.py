b987ae4d11861fd7ca9dc8a427eeb058403c54f0
Jussi17 <143203799+Jussi17@users.noreply.github.com>
2023-12-01 18:06:44 +0200

    Add files via upload

---
+++ models.py
@@ -0,0 +1,47 @@
+from django.db import models
+from django.urls import reverse
+from django.conf import settings
+
+class ProductManager(models.Manager):
+    def get_queryset(self):
+        return super(ProductManager, self).get_queryset().filter(is_active=True)
+
+class Category(models.Model):
+    name = models.CharField(max_length=255, db_index=True)
+    slug = models.SlugField(max_length=255, unique=True)
+
+    class Meta:
+        verbose_name_plural = 'categories'
+
+    def get_absolute_url(self):
+        return reverse('store:category_list', args=[self.slug])
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    category = models.ForeignKey(Category, related_name='product', on_delete=models.CASCADE)
+    title = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+    image = models.ImageField(upload_to='images/', default='images/default.jpg')
+    slug = models.SlugField(max_length=255)
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    in_stock = models.BooleanField(default=True)
+    is_active = models.BooleanField(default=True)
+    is_shoe = models.BooleanField(default=False)
+    is_asuste = models.BooleanField(default=False)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    objects = models.Manager()
+    products = ProductManager()
+
+    class Meta:
+        verbose_name_plural = 'Products'
+        ordering = ('-created',)
+
+    def get_absolute_url(self):
+        return reverse('store:product_detail', args=[self.slug])
+
+    def __str__(self):
+        return self.title
